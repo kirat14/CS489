c6115a3da3f172be879126ef3b3965043fc1e7f9
Tarik <moumini.tarik@gmail.com>
2024-04-04 06:56:32 -0500

    first commit

---
+++ lab1b/src/main/java/com/tarik/moumini/App.java
@@ -0,0 +1,13 @@
+package com.tarik.moumini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
